0e51bcc809a155d908e74082c02c2742404ef108
HossamOkasha <enghossamokashaa@gmail.com>
2020-09-18 16:33:11 +0200

    distinguish the section in view-

--- js/app.js
+++ js/app.js
@@ -22,3 +22,19 @@ const buildNav = () => {
 }
 buildNav();
 /* ---End of the nav--- */
+
+/* ---Add functionality to distinguish the section in view---*/
+// Add class 'active' to section when near(~200px) top of viewport
+// and remove the class from the section if it out of the viewport
+window.addEventListener("scroll", function () {
+    sections.forEach(section => {
+        const offset = window.pageYOffset - section.offsetTop;
+        const viewOut = .6*section.clientHeight;
+        if (offset <= viewOut && offset >= -150) {
+            section.classList.add("active__section");            
+        } else {
+            section.classList.remove("active__section");
+        }
+    });
+});
+/* ---End of distinguishing section--- */
