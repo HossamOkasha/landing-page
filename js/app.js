916f58466db4f6638972d985a8552edb995bd9a2
HossamOkasha <enghossamokashaa@gmail.com>
2020-09-18 16:55:09 +0200

    scroll to sections

--- js/app.js
+++ js/app.js
@@ -23,18 +23,31 @@ const buildNav = () => {
 buildNav();
 /* ---End of the nav--- */
 
-/* ---Add functionality to distinguish the section in view---*/
+
+/* ---distinguish the section in view---*/
 // Add class 'active' to section when near(~200px) top of viewport
 // and remove the class from the section if it out of the viewport
 window.addEventListener("scroll", function () {
     sections.forEach(section => {
         const offset = window.pageYOffset - section.offsetTop;
         const viewOut = .6*section.clientHeight;
-        if (offset <= viewOut && offset >= -150) {
+        if (offset <= viewOut && offset >= (-viewOut+50) ){
             section.classList.add("active__section");            
         } else {
             section.classList.remove("active__section");
         }
     });
 });
-/* ---End of distinguishing section--- */
+/* ---End of distinguishing the section--- */
+
+
+/* ---Scroll to Sections--- */
+navList.addEventListener("click", e => {
+    // using the nav dataset in the items
+    const sec = document.querySelector(`#${e.target.dataset.navscroll}`);
+    window.scrollTo({
+        top: sec.offsetTop - 100, // (-100) to avoid fixed navbar
+        behavior: "smooth",
+    });
+});
+/* ---End of Scroll to Sections--- */
